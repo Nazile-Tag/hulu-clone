e4af5c7befda9b7d4995e682ea1a55dcd02df560
Nazile-Tag <tagnazile@gmail.com>
2022-03-01 23:26:35 +0300

    finished

--- components/Results.js
+++ components/Results.js
@@ -1,13 +1,14 @@
 import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
 
 
 function Results({ results }) {
   return (
-    <div>
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {results.map(result => (
         <Thumbnail key={result.id} result={result} />
       ))}
-    </div>
+    </FlipMove>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      }
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 };
